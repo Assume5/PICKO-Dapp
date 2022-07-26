d5ed6d5e48a3fe40b4f923c9180b82b4a84fa9cf
Chenyi <zou.chenyi@yahoo.com>
2022-07-25 23:58:55 -0400

    Add routing machine leaflet

--- client/src/components/Owner/NewOrderModal/NewOrderModal.tsx
+++ client/src/components/Owner/NewOrderModal/NewOrderModal.tsx
@@ -1,6 +1,7 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   newOrderModal: boolean;
@@ -8,12 +9,18 @@ interface Props {
 }
 
 export const NewOrderModal: React.FC<Props> = ({ setNewOrderModal, newOrderModal }) => {
+  const navigation = useNavigate();
   return (
     <div className={`modal new-order-modal ${newOrderModal ? 'show' : ''}`}>
       <FontAwesomeIcon icon={faTimes} className="close-button" onClick={() => setNewOrderModal(false)} />
 
       <div className="modal-inner">
-        <div className="order-circle">
+        <div
+          className="order-circle"
+          onClick={() => {
+            navigation('/owner');
+          }}
+        >
           <p>
             <strong>15</strong> New Orders
           </p>
--- client/src/pages/Customer/Map/Map.tsx
+++ client/src/pages/Customer/Map/Map.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-import L from 'leaflet';
+import 'leaflet-routing-machine';
+
+import L, { IconOptions } from 'leaflet';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+
 export const Map = () => {
   const [latLong, setLatLong] = useState<[number, number]>();
   const [map, setMap] = useState<L.Map>();
@@ -8,6 +12,14 @@ export const Map = () => {
   const clientDot = L.icon({
     iconUrl: '/imgs/dot.svg',
     iconSize: [32, 16],
+  });
+  const storeIcon = L.icon({
+    iconUrl: '/imgs/restaurant-icon.svg',
+    iconSize: [32, 32],
+  });
+  const driverIcon = L.icon({
+    iconUrl: '/imgs/driver-car-icon.svg',
+    iconSize: [32, 20],
   });
   useEffect(() => {
     if (!navigator.geolocation) {
@@ -30,8 +42,46 @@ export const Map = () => {
   useEffect(() => {
     if (!latLong) return;
     if (!map) return;
+    const waypoints = [L.latLng(latLong[0], latLong[1]), L.latLng(42.9920483, -78.8195213)];
+
+    const plan = new L.Routing.Plan(waypoints, {
+      createMarker: (i, wp, nWps) => {
+        let icon: L.Icon<IconOptions> | null = null;
+        if (i === 0) {
+          icon = driverIcon;
+        } else if (i === nWps - 1) {
+          icon = storeIcon;
+        } else {
+          icon = clientDot;
+        }
+        return L.marker(wp.latLng, {
+          icon: icon,
+        });
+      },
+    });
+
+    const instance = new L.Routing.Control({
+      waypoints: waypoints,
+      routeWhileDragging: false,
+      addWaypoints: false,
+      lineOptions: {
+        styles: [{ color: 'white', opacity: 1, weight: 3 }],
+        missingRouteStyles: [
+          { color: 'black', opacity: 0.15, weight: 7 },
+          { color: 'white', opacity: 0.6, weight: 4 },
+          { color: 'gray', opacity: 0.8, weight: 2, dashArray: '7,12' },
+        ],
+        addWaypoints: false,
+        extendToWaypoints: true,
+        missingRouteTolerance: 0,
+      },
+      plan,
+    }).addTo(map);
 
     map.flyTo([latLong[0], latLong[1]]);
+    return () => {
+      map.removeControl(instance);
+    };
   }, [latLong]);
 
   if (!latLong) return null;
@@ -40,7 +90,7 @@ export const Map = () => {
     <div className="leaflet-map" id="mapid" style={{ height: '100vh' }}>
       <MapContainer
         center={[latLong[0], latLong[1]]}
-        zoom={17}
+        zoom={20}
         scrollWheelZoom={false}
         style={{ height: '100%' }}
         whenCreated={(map) => setMap(map)}
@@ -49,7 +99,6 @@ export const Map = () => {
           attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
           url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
         />
-        <Marker position={[latLong[0], latLong[1]]} icon={clientDot}></Marker>
       </MapContainer>
     </div>
   );
